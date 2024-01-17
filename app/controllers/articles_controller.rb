8c2698d42c5b3620dab51c86ca27963f3063fe05
Jayeon Hwang <jayeon512@gmail.com>
2024-01-17 14:00:15 -0600

    add api key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-   
+    api_key = Rails.application.credentials.api_key
+    response = HTTP.get("https://newsapi.org/v2/everything?q=cat&apiKey=#{api_key}")
+    render json: response.parse(:json)
   end
 end
